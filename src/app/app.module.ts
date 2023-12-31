a8d586f369f89903f01ff99eecb4d3fa888a4540
Ahmed-Abdelhamee <medoabdelhameed@gmail.com>
2023-12-31 17:09:24 +0200

    #.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,40 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ZfooterComponent } from './components/zfooter/zfooter.component';
+import { HomeComponent } from './components/home/home.component';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { AccessoiresComponent } from './components/accessoires/accessoires.component';
+import { BagsComponent } from './components/bags/bags.component';
+import { BrandsComponent } from './components/brands/brands.component';
+import { ClothingComponent } from './components/clothing/clothing.component';
+import { JewelleryComponent } from './components/jewellery/jewellery.component';
+import { NewComponent } from './components/new/new.component';
+import { SaleComponent } from './components/sale/sale.component';
+import { ShoesComponent } from './components/shoes/shoes.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ZfooterComponent,
+    HomeComponent,
+    SaleComponent,
+    NewComponent,
+    BrandsComponent,
+    ClothingComponent,
+    ShoesComponent,
+    BagsComponent,
+    AccessoiresComponent,
+    JewelleryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
