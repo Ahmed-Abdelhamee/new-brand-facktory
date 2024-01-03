9869e5c248dc39d289306205bdb7ab7b4a3f1a48
Ahmed-Abdelhamee <medoabdelhameed@gmail.com>
2024-01-03 16:41:31 +0200

    #.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from "@angular/core";
 import { RouterModule , Routes } from "@angular/router";
 import { HomeComponent } from "./components/home/home.component";
 import { WomenComponent } from "./components/women/women.component";
+import { KidsComponent } from "./components/kids/kids.component";
+import { MenComponent } from "./components/men/men.component";
 
 const routes: Routes = [
     // {path:"",redirectTo:"/home",pathMatch:"full"},
     {path:"",component:WomenComponent},
     {path:"home",component:HomeComponent},
+    {path:"men",component:MenComponent},
+    {path:"women",component:WomenComponent},
+    {path:"kids",component:KidsComponent},
 ]
 
 @NgModule({
