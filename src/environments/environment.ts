61b1c898a8de7ae7bde9f46027229f72eb68f606
Ahmed-Abdelhamee <medoabdelhameed@gmail.com>
2024-01-18 14:09:12 +0200

    #.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
   firebase: {
-    projectId: 'ecommerce-625b8',
-    appId: '1:863283674897:web:7d4ed964ebf568af7dd6b0',
-    databaseURL: 'https://ecommerce-625b8-default-rtdb.firebaseio.com',
-    storageBucket: 'ecommerce-625b8.appspot.com',
+    projectId: 'ecommerce-e3827',
+    appId: '1:626822506527:web:6c0b3f162a0bdddd968d2a',
+    databaseURL: 'https://ecommerce-e3827-default-rtdb.firebaseio.com',
+    storageBucket: 'ecommerce-e3827.appspot.com',
     locationId: 'us-central',
-    apiKey: 'AIzaSyBz6iGoAw4_SBacOOjlm9SpfxHo5qTNywQ',
-    authDomain: 'ecommerce-625b8.firebaseapp.com',
-    messagingSenderId: '863283674897',
-    measurementId: 'G-EYK19N6C1P',
+    apiKey: 'AIzaSyDU7cUE3msSkkdhnmwDRAIZtjDYklASXqw',
+    authDomain: 'ecommerce-e3827.firebaseapp.com',
+    messagingSenderId: '626822506527',
+    measurementId: 'G-FVM5NMBLK4',
   },
 };
